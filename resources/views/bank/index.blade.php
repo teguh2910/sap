723b92c90a0dfb3b5af6f7d50392ebebd7b313ac
teguh2910 <teguh.yuhono@student.unsia.ac.id>
2023-07-15 21:52:58 +0700

    add view index bank

---
+++ resources/views/bank/index.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.app')
+
+
+@section('content')
+
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      @if ($message = Session::get('success'))
+        <div class="alert alert-success alert-block">
+          <button type="button" class="close" data-dismiss="alert">×</button>	
+            <strong>{{ $message }}</strong>
+        </div>
+      @endif
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">List Bank</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">List Bank</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+        <a href="" class="btn btn-sm btn-success">Create</a>
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">          
+          <!-- /.col-md-12 -->
+          <div class="col-md-12">
+          <div class="card">
+              <!-- /.card-header -->
+              <div class="card-body">                
+                <table id="example1" class="table table-bordered table-striped">
+                  <thead>
+                  <tr>
+                    <th>id_bank</th>
+                    <th>nama_bank</th>
+                    <th>cabang_bank</th>
+                    <th>currency_bank</th>
+                    <th>action</th>                    
+                  </tr>
+                  </thead>
+                  <tbody>
+                  </tbody>                  
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            </div>
+            <!-- /.col-md-12 -->
+        </div>
+        <!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content -->
+  
+@endsection
