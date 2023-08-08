f026c3e8cd62aaefbbbb47304f3cbb3e718d9571
teguh2910 <teguh.yuhono@student.unsia.ac.id>
2023-08-08 21:08:47 +0700

    dsfs

--- resources/views/dashboard/po_customer.blade.php
+++ resources/views/dashboard/po_customer.blade.php
@@ -59,7 +59,8 @@
                     <th>Qty Oustanding</th>
                     <th>Uom</th>
                     <th>Price Oustanding</th>
-                    <th>Amount Oustanding</th>                                        
+                    <th>Amount Oustanding</th>  
+                    <th></th>                                      
                   </tr>
                   </thead>
                   <tbody>
@@ -94,7 +95,7 @@
                       <td>{{ $g->uom }}</td>
                       <td>Rp {{ $d->harga_po - $g->harga_gr }}</td>
                       <td>Rp {{ ($d->qty_po*$d->harga_po) - ($g->harga_gr*$g->qty_gr) }}</td>
-                        
+                      <td><a href="" class="btn btn-xs btn-success">Closed</a><a href="" class="btn btn-xs btn-danger">Cance</a></td>
                     </tr>
                     @endforeach
                   </tbody>                  
